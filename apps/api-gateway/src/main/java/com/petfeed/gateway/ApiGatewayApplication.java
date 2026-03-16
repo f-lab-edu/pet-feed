445745c521caa369f82c96c61bdd6836be93dd1e
me <mothership2002@naver.com>
2026-03-16 19:41:10 +0900

    api-gateway 의존성 추가 및 설정 수정 - spring-cloud-dependencies BOM 추가 - Feign 의존성 및 설정 추가 (@EnableFeignClients) - JWT 관련 의존성 추가 - user-service URL 설정 추가 (application.yml) - common-domain 모듈에 jackson-databind 의존성 추가

--- apps/api-gateway/src/main/java/com/petfeed/gateway/ApiGatewayApplication.java
+++ apps/api-gateway/src/main/java/com/petfeed/gateway/ApiGatewayApplication.java
@@ -2,8 +2,10 @@ package com.petfeed.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication(scanBasePackages = "com.petfeed")
+@EnableFeignClients(basePackages = "com.petfeed.gateway.client")
 public class ApiGatewayApplication {
 
     public static void main(String[] args) {
